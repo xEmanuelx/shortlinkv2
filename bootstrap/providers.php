3076c308dcca26ad2391c47b65227365bc701578
xEmanuelx <100787520+xEmanuelx@users.noreply.github.com>
2025-10-07 10:52:04 +0200

    Add files via upload

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\HelperServiceProvider::class,
+    App\Providers\MacroServiceProvider::class,
+];
